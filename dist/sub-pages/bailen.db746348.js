76f85df7a66ad0cf16fa34f1954c19f85cb9336f
Xavi Serrano <xserrano2001@gmail.com>
2024-11-03 18:49:31 +0100

    Quitar barra scroll nav

---
+++ dist/sub-pages/bailen.db746348.js
@@ -0,0 +1,15 @@
+// script.js
+const swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    }
+});
+
+//# sourceMappingURL=bailen.db746348.js.map
