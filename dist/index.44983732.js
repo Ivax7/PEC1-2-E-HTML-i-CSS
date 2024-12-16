603a7c5a578d9855a7be6a03748d4fbffac7c3e1
Xavi Serrano <xserrano2001@gmail.com>
2024-12-16 17:31:17 +0100

    Final commit

---
+++ dist/index.44983732.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const menuToggle = document.querySelector(".sidebar__menu-toggle");
+    const sidebar = document.querySelector(".sidebar");
+    menuToggle.addEventListener("click", ()=>{
+        sidebar.classList.toggle("active");
+    });
+});
+
+//# sourceMappingURL=index.44983732.js.map
